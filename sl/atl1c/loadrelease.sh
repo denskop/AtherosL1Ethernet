ebf18a2a2bd25b90e7687e5804d4ef94fd7db602
denskop <denskop@mail.ru>
2018-03-22 22:44:49 +0300

    Initial commit

---
+++ sl/atl1c/loadrelease.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+cd build
+cd Release
+echo "file attribute adjusting for  AtherosL1cEthernet.kext"
+
+sudo chown -R root:wheel AtherosL1cEthernet.kext
+sudo find AtherosL1cEthernet.kext -type d -exec chmod 0755 {} \;
+sudo find AtherosL1cEthernet.kext -type f -exec chmod 0644 {} \; 
+
+sudo kextload -t AtherosL1cEthernet.kext
+
+
